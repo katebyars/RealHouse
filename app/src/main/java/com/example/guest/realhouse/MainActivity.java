57482f85c118dd365f8619c61b64add514e5b547
Kate Byars <katebyars5@gmail.com>
2017-11-06 14:42:57 -0800

    add logout logic.

--- app/src/main/java/com/example/guest/realhouse/MainActivity.java
+++ app/src/main/java/com/example/guest/realhouse/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.guest.realhouse;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.example.guest.realhouse.R;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.my_routes:
                     mTextMessage.setText("My Routes");
                     return true;
+                case R.id.logout:
+                    mTextMessage.setText("Logout");
+                    logout();
+                    return true;
             }
             return false;
         }
@@ -41,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
 }
